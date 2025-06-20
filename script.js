1f1312eb0e6131f0e272b8150d733ed88d62d851
Phillips <rheaphillips08@gmail.com>
2025-06-20 10:00:46 -0400

    Fix logic for respawning obstacles, fix positioning of the shopping cart and obstacles, and make the game begin sfter the spacebar is clicked for the first time

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let background = document.querySelector('.background'), playerElem = document.querySelector('.player');
 let border = 10;
+let paused = true;
 
 // the position of the origin (inital values as per the posiion of the wall and ground in the DOM)
 let origin = [border, background.clientHeight - playerElem.clientHeight - border];
@@ -19,33 +20,27 @@ const player = {
     vel: 0, // vertical velocity
     y: 0,
     g: 700, // gravitational acceleration
-    count: 0, // number of spacebar clicks
+    
     jumping: false,
     jumpHoldTime: 0,
-    maxJumpHold: 0.3, // Max time (in seconds) holding jump gives more height
-
+    maxJumpHold: 0.01, // Max time (in seconds) holding jump gives more height
     freeFall: false, // when the player is mid-air and it's velocity can no longer be increased
 
 
     // the velocity is only increased if it's been increased less than two times since the player got off the ground, and the user hasn't let go of the spacebar (indicated by the date of this.freeFall)
     jumpStart() {
-        if (this.count < 2) {
-            this.count++;
+        if (!this.jumping) {
             this.jumping = true;
+            this.vel += 100; // initial jump impulse
             this.jumpHoldTime = 0;
-            this.vel += 500; // initial jump impulse
         }
     },
 
     jumpHold(dt) {
         if (this.jumping && this.jumpHoldTime < this.maxJumpHold) {
-            this.vel += 250 * dt; // continue to increase velocity
+            this.vel += 200; // continue to increase velocity
             this.jumpHoldTime += dt;
         }
-    },
-
-    jumpEnd() {
-        this.jumping = false;
     },
     
     // updates the player's velocity and position
@@ -82,45 +77,42 @@ class Obstacle {
             console.warn("Could not find valid restart position.");
             break; // prevent infinite loop
         }
-    } while (obstacles.some( obstacle => this != obstacle && Math.abs(this.x - obstacle.x) < 400));
+    } while (obstacles.some( obstacle => this == obstacle || Math.abs(this.x - obstacle.x) < 300));
   }
 
   move() {
-    this.x--;
+    this.x -= 1;
     this.id.style.left = xCoord(this.x);
-    if (this.x < -this.width - 10) {
-        this.restart();
-    }
+    if (this.x < -this.width - 10) this.restart();
   }
 }
 
 const obstacles = [];
-
-for (let i = 0; i < 3; i++) {
-    obstacles.push(new Obstacle(`${i + 1}`));
-}
+for (let i = 0; i < 3; i++) obstacles.push(new Obstacle(`${i + 1}`));
+obstacles.forEach(obstacle => obstacle.restart());
 
 // player jumps when spacebar is pressed
 window.addEventListener('keydown', function (e) {
     if (e.key === " ") {
+        if (paused) paused = false;
         player.jumpStart();
-    }
-});
-
-// player stops jumping when spacebar is released
-window.addEventListener('keyup', function (e) {
-    if (e.key === " ") {
-        player.jumpEnd();
     }
 });
 
 // the velocity and postion, and the UI is updated in accordance, every 10 ms
 let lastTime = performance.now();
+let totalTime = 0;
 window.setInterval(function () {
     let now = performance.now();
     let dt = (now - lastTime) / 1000; // seconds
     lastTime = now;
-    player.updateVelocityAndPosition(dt);
-    obstacles.forEach(obstacle => obstacle.move());
+    if (paused) {
+        
+    } else {
+        totalTime += dt;
+        player.updateVelocityAndPosition(dt);
+        if (totalTime > 2) {
+            obstacles.forEach(obstacle => obstacle.move());
+        }
+    }
 }, 5);
-    
